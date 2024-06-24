d23d581f7345f55319778b6705049e70ccecdc9a
victorlimas <victorlimasantos99@hotmail.com>
2024-06-24 19:18:34 -0300

    uploading the project

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue()]
+});
