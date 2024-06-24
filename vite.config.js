9f0b06ce3d0eca02a4f018299318eedb83975619
victorlimas <victorlimasantos99@hotmail.com>
2024-06-24 00:05:33 -0300

    uploading the project

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
+import InjectCSS from '@itsy/vite-css-inject';
+import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    react(),
+    InjectCSS(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@tests": path.resolve(__dirname, "./tests")
+    }
+  }
 })
